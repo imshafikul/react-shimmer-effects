2b8d8051edd258d3d0491ab3f355d10b6bdbdb93
Shafikul Islam <shafikul.me@gmail.com>
2021-06-30 16:14:30 +0600

    Card styling move to component directory

--- src/components/common/Card/Card.js
+++ src/components/common/Card/Card.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import classNames from "classnames";
+import "./card.scss";
 
 const Card = ({ children, cssClasses }) => {
   return (
