974853a79aed267f622cb2d0328810c73eb29702
Shafikul Islam <shafikul.me@gmail.com>
2021-07-15 20:29:43 +0600

    Card paddingsize prop

--- src/components/common/Card/Card.js
+++ src/components/common/Card/Card.js
@@ -3,12 +3,13 @@ import PropTypes from "prop-types";
 import classNames from "classnames";
 import "./card.scss";
 
-const Card = ({ children, cssClasses }) => {
+const Card = ({ children, cssClasses, paddingSize }) => {
   if (!children) return null;
   return (
     <div
       className={classNames({
         "shimmer-card": true,
+        [`p-${paddingSize}`]: paddingSize,
         [cssClasses]: cssClasses,
       })}
     >
@@ -21,8 +22,9 @@ Card.propTypes = {
   cssClasses: PropTypes.string,
   children: PropTypes.oneOfType([PropTypes.element, PropTypes.array])
     .isRequired,
+  paddingSize: PropTypes.oneOf([false, 20, 30]),
 };
 
-Card.defaultProps = {};
+Card.defaultProps = { paddingSize: false };
 
 export default Card;
